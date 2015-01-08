d47d10a31996ae6955d68a762aee47afb9d5f011
juno <juno@krasava.fi>
2015-01-08 03:24:26 +0200

    dev

--- src/app/app.go
+++ src/app/app.go
@@ -1,10 +1,49 @@
-package main 
+package main
 
 import (
-
+	"log"
+	"log/syslog"
+	"net"
+	"net/http"
+	"net/http/fcgi"
+	"startones"
+	"sync"
 )
 
-func main() {
+var startOnce sync.Once
+var startparameters []string
+
+type FastCGIServer struct{}
+
+func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+
+	golog, err := syslog.New(syslog.LOG_ERR, "golog")
+
+	defer golog.Close()
+	if err != nil {
+		log.Fatal("error writing syslog!!")
+	}
+
+	//	themes := req.Header.Get("X-THEMES")
+	//	locale := req.Header.Get("X-LOCALE")
+	//	variant := req.Header.Get("X-VARIANT")
+	//	site := req.Header.Get("X-DOMAIN")
+	//	pathinfo := req.Header.Get("X-PATHINFO")
+	//	bot := req.Header.Get("X-BOT")
+
+	startOnce.Do(func() {
+		startparameters = startones.Start(*golog)
+	})
 
 }
 
+func main() {
+
+	listener, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	srv := new(FastCGIServer)
+	fcgi.Serve(listener, srv)
+
+}
