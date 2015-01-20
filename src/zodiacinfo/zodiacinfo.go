b8138c3e3db7ba5e7eafa109121768b07e341c1f
juno <juno@krasava.fi>
2015-01-20 02:33:08 +0200

    prod

--- src/zodiacinfo/zodiacinfo.go
+++ src/zodiacinfo/zodiacinfo.go
@@ -8,6 +8,8 @@ import (
 
 func GetInfo(golog syslog.Writer, c redis.Conn, locale string, themes string, site string, path string) []byte {
 
+	var tryget bool = false
+
 	var bzodiacinfo []byte
 
 	var redlink string = locale + ":" + themes + ":" + site + ":" + path
@@ -19,22 +21,49 @@ func GetInfo(golog syslog.Writer, c redis.Conn, locale string, themes string, si
 	} else {
 
 		if exist == 0 {
-			
-			
+
+			golog.Info("site " + site)
 
 			splitsite := strings.Split(site, ".")
-			onlydomain := splitsite[len(splitsite)-2] + "." + splitsite[len(splitsite)-1]
-			redlink = locale + ":" + themes + ":" + onlydomain + ":" + path
+
+			if len(splitsite) > 1 {
+
+				splitsite := strings.Split(site, ".")
+				onlydomain := splitsite[len(splitsite)-2] + "." + splitsite[len(splitsite)-1]
+				redlink = locale + ":" + themes + ":" + onlydomain + ":" + path
+
+			}
+
+			if exist, err := redis.Int(c.Do("EXISTS", redlink)); err != nil {
+
+				golog.Err("zodiacinfo:exist " + err.Error())
+
+			} else {
+
+				if exist == 1 {
+					
+					tryget = true
+
+				}
+
+			}
+
+		} else if exist == 1 {
+
+			tryget = true
 
 		}
 
-		if resualt, err := redis.Bytes(c.Do("GET", redlink)); err != nil {
+		if tryget {
 
-			golog.Err("zodiacinfo " + err.Error())
-		} else {
+			if resualt, err := redis.Bytes(c.Do("GET", redlink)); err != nil {
 
-			bzodiacinfo = resualt
+				golog.Err("zodiacinfo " + err.Error())
+			} else {
 
+				bzodiacinfo = resualt
+
+			}
 		}
 
 	}
